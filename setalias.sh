d004b885bf1a9e09c2b1d29f90814749200ac611
Rudranil <e.camerapid@gmail.com>
2025-12-13 02:40:49 +0530

    Create setalias.sh to manage zsh aliases
    This script displays current aliases and prompts the user to add new ones permanently to their zsh configuration.

---
+++ setalias.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+echo  "The aliases available for you Currently are :\n"
+$(alias) | echo 	#this command shows the currently set aliases
+read -p "Do you want to add more aliases permanently?   y/n  " choice 	#choice stores the choice :)
+if [ "$choice" = 'y' ] || [ "$choice" = 'Y' ] || [ "$choice" = 'yes' ] || [ "$choice" = 'Yes' ] ; then
+	echo "PLEASE ADD ALIAS OF YOUR CHOICE IN THE FILE ABOUT TO OPEN\n IN the format --  \e[1m  alias chosen_name='original_command' \e[0m\n into any line after first two (\e[4mpreferred very last lines\e[0m) And make sure to save the file\n"
+	read -p "Press enter to continue" forWaiting   #this line is only to let user see the previous line until they are done reading  
+	gedit ~/.zshrc  &&	#open the alias file for zsh shell
+	echo  "\nThe file containing aliases is ~/.zshrc  (~/.bashrc or ~/.bash_aliases (for bash))\nType alias to view aliases\n\e[31mIf shows source not found... type 'source ~/.zsh_aliases' to update\n(Auto effective after restart)\e[0m" 
+	echo "\e[1;31m\n This will only work in zsh shell. for any issue please check out https://github.com/rudranil5/SetPermanentAlias\e[0m\n" &&
+	source ~/.zshrc	# source the file for zsh
+	
+	# && after command ensures next command is not executed if the previous is not
+else
+	:  # : just skips it, does nothing
+fi
