6b60a74e3da306eb4d0a971139a6913690f4b43b
Rudranil <e.camerapid@gmail.com>
2025-12-14 01:24:37 +0530

    Update setalias.sh

--- setalias.sh
+++ setalias.sh
@@ -6,8 +6,9 @@ if [ "$choice" = 'y' ] || [ "$choice" = 'Y' ] || [ "$choice" = 'yes' ] || [ "$ch
 	echo "PLEASE ADD ALIAS OF YOUR CHOICE IN THE FILE ABOUT TO OPEN\n IN the format --  \e[1m  alias chosen_name='original_command' \e[0m\n into any line after first two (\e[4mpreferred very last lines\e[0m) And make sure to save the file\n"
 	read -p "Press enter to continue" forWaiting   #this line is only to let user see the previous line until they are done reading  
 	gedit ~/.zshrc  &&	#open the alias file for zsh shell
-	echo  "\nThe file containing aliases is ~/.zshrc  (~/.bashrc or ~/.bash_aliases (for bash))\nType alias to view aliases\n\e[31mIf shows source not found... type 'source ~/.zsh_aliases' to update\n(Auto effective after restart)\e[0m" 
-	echo "\e[1;31m\n This will only work in zsh shell. for any issue please check out https://github.com/rudranil5/SetPermanentAlias\e[0m\n" &&
+	echo "\e[1m\n This will only work in zsh shell. for any issue please check out https://github.com/rudranil5/SetPermanentAlias\e[0m\n" &&
+	echo  "\nThe file containing aliases is ~/.zshrc  (~/.bashrc or ~/.bash_aliases (for bash))\nType alias to view aliases\n\e[31mIf shows source not found... type 'source ~/.zsh_aliases' to update\n(Auto effective after restart)\e[0m" &&
+	
 	source ~/.zshrc	# source the file for zsh
 	
 	# && after command ensures next command is not executed if the previous is not
